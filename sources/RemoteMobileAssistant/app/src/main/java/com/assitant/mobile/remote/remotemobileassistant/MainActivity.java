0ac6ada9e72bd237ff112b583330de92a822f62b
Andrei Bogdan Baran <andrei.bogdan.baran@gmail.com>
2016-05-26 22:41:18 +0300

    Added Android Project with Login/Logout and Base MainActivity

---
+++ sources/RemoteMobileAssistant/app/src/main/java/com/assitant/mobile/remote/remotemobileassistant/MainActivity.java
@@ -0,0 +1,16 @@
+package com.assitant.mobile.remote.remotemobileassistant;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActvity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_main;
+    }
+}
